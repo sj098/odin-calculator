3aace07d4fda06ef763e038335becfb938861bd5
Suvin Jeon <suvinjeon@gmail.com>
2022-09-27 16:04:57 -0400

    Add calculating functions

--- script.js
+++ script.js
@@ -30,3 +30,78 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+// DISPLAY
+// Create the functions that populate the display when you 
+// click the number buttons. You should be storing the 
+// ‘display value’ in a variable somewhere for use in the 
+// next step.
+let currentValue = '';
+let previousValue = '';
+let operator = '';
+
+document.addEventListener('DOMContentLoaded', function() {
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector('.equal');
+    let numbers = document.querySelectorAll('.digit');
+    let operators = document.querySelectorAll('.operator');
+    let previousScreen = document.querySelector('.previous');  
+    let currentScreen = document.querySelector('.current');
+
+    numbers.forEach((num) => num.addEventListener('click', function(e) {
+        show(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener('click', function(e) {
+        operate(e.target.textContent)
+        previousScreen.textContent = previousValue + ' ' + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener('click', function() {
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = '0';
+        currentScreen.textContent = '0';
+    })
+
+    equal.addEventListener('click', function(e) {
+        if (currentValue != '' && previousValue != '') {
+            calculate();
+            previousScreen.textContent = '';
+            currentScreen.textContent = previousValue;
+        }
+    })
+})
+
+function show(num) {
+    if (currentValue.length <= 18) {
+        currentValue += num;
+    }
+}
+
+function operate(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if (operator === '+') {
+        previousValue += currentValue;
+    } else if (operator === '-') {
+        previousValue -= currentValue;
+    } else if (operator === 'x') {
+        previousValue *= currentValue;
+    } else if (operator === '/') {
+        previousValue /= currentValue;     
+    }
+
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString(); 
+}
